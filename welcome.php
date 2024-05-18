036fe5697facea903b226137003ef1a50bfbb9f3
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-05-19 00:34:24 +0300

    add the clear button at login form

--- welcome.php
+++ welcome.php
@@ -7,7 +7,8 @@ error_log($_SESSION['email'], 3, 'php://stderr');
 // Check if the username session variable is set
 if (isset($_SESSION['email'])) {
     $email = htmlspecialchars($_SESSION['email']);
-    echo "Hi " . $email;
+    echo "Hello :  " . $email . "<br>";
+    echo "<p> <a href='login.html'>&lt;GO BACK&gt;</a></p>";
 } else {
     // Redirect to login page if accessed directly without logging in
     header("Location: index.html");
